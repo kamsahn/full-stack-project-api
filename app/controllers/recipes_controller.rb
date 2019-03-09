48a75b95749d7556fb515548e8ddd499da5156de
kamsahn <samjkahn@gmail.com>
2019-03-08 23:27:24 -0500

    Change recipe back to OpenRead
    - use for development ease

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,11 @@
-class RecipesController < ProtectedController
+class RecipesController < OpenReadController
   before_action :set_recipe, only: [:show, :update, :destroy]
 
   # GET /recipes
   def index
-    @recipes = current_user.recipes
+    @recipes = Recipe.all
+    # use when client is ready
+    # @recipes = current_user.recipes
 
     render json: @recipes
   end
