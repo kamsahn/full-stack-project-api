65dfe97c511b18c2ebd19bef2b370b2782ed829b
kamsahn <samjkahn@gmail.com>
2019-03-08 23:13:58 -0500

    Update curl scripts for ingredients
    - make compatible with token auth

--- curl-scripts/ingredients/get-ingredient.sh
+++ curl-scripts/ingredients/get-ingredient.sh
@@ -3,5 +3,6 @@
 curl "http://localhost:4741/ingredients/${ID}" \
   --include \
   --request GET \
+  --header "Authorization: Token token=${TOKEN}" \
 
 echo
--- curl-scripts/ingredients/update-ingredient.sh
+++ curl-scripts/ingredients/update-ingredient.sh
@@ -4,6 +4,7 @@ curl "http://localhost:4741/ingredients/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "ingredient": {
       "name": "'"${NAME}"'"
