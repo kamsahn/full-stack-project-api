420bfb2c2de229e33aea192a59a56668622e5d1e
kamsahn <samjkahn@gmail.com>
2019-03-08 22:03:21 -0500

    Add relationship between recipe and ingredients
    - one to many
    - added references in ingredient table and migrated
    - edited both models
    - edited recipe serializer to show ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_024332) do
+ActiveRecord::Schema.define(version: 2019_03_09_025327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_03_09_024332) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipe_id"
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -48,5 +50,6 @@ ActiveRecord::Schema.define(version: 2019_03_09_024332) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "ingredients", "recipes"
   add_foreign_key "recipes", "users"
 end
