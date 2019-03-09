150f1d9b1231523398f4b98202cb5fd7f8c50768
kamsahn <samjkahn@gmail.com>
2019-03-08 22:39:55 -0500

    Add amount column to meal
    - edit serializer for recipe to display ingredient and amount

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,13 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :ingredients
+  attributes :id, :name, :ingredients_list
+
+  def ingredients_list
+    object.meals.map do |meal|
+      {
+        # unit: meal.unit,
+        amount: meal.amount,
+        ingredient: meal.ingredient.name
+      }
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_032015) do
+ActiveRecord::Schema.define(version: 2019_03_09_033344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_032015) do
     t.bigint "ingredient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "amount"
     t.index ["ingredient_id"], name: "index_meals_on_ingredient_id"
     t.index ["recipe_id"], name: "index_meals_on_recipe_id"
   end
