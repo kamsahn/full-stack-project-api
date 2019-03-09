ba89ed3e4649c1ad8594bb47521b9acd1d96ddac
kamsahn <samjkahn@gmail.com>
2019-03-08 21:52:08 -0500

    Create curl scripts for ingredients
    -all crud, less new and edit

---
+++ curl-scripts/ingredients/create-ingredient.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl "http://localhost:4741/ingredients" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "ingredient": {
+      "name": "'"${NAME}"'"
+    }
+  }'
+
+echo
